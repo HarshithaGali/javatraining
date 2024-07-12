e0a482b5ddd8b3137cf404a214647b7258b877e8
HarshithaGali <115328369+HarshithaGali@users.noreply.github.com>
2024-07-11 22:17:59 -0500

    Create Name.java

---
+++ src/javatraining/Name.java
@@ -0,0 +1,17 @@
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	char ch1='\u0C39';
+	char ch2='\u0C4D';
+	char ch3='\u0C30';
+	char ch4='\u0C37';
+	char ch5='\u0C3F';
+	char ch6='\u0C24';
+	System.out.println(ch1+""+ch3+""+ch2+""+ch4+""+ch5+""+ch6);
+
+	}
+
+}
