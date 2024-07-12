09e18a47ce8d73da11561948eff2a7e8d8ea77dc
HarshithaGali <115328369+HarshithaGali@users.noreply.github.com>
2024-07-11 22:46:19 -0500

    Create GreatestNumber.java

---
+++ src/javatraining/GreatestNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a=sc.nextInt();
+		System.out.println("Enter the second number");
+		int b=sc.nextInt();
+		System.out.println("Enter the third number");
+		int c=sc.nextInt();
+		if(a>b && a>c)
+		{
+			System.out.println(a);
+		}
+		else if(b>c)
+		{
+			System.out.println(b);
+		}
+		else
+			System.out.println(c);
+		
+
+	}
+
+}
